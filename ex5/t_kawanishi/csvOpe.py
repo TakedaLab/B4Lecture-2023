10e6cb3d084667090b781659b64294daeb0bea1e
980055617 <y980055617@gmail.com>
2023-05-24 23:01:21 +0900

    update file

--- ex5/t_kawanishi/csvOpe.py
+++ ex5/t_kawanishi/csvOpe.py
@@ -1,40 +1,76 @@
+"""csv operation."""
 import csv
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_data(data:np.ndarray):
+
+def plot(data: np.ndarray, centroid: np.ndarray, label: np.ndarray, g_num):
     """To plot data
 
     Args:
         data (np.ndarray): dataset for plot
+        centroid(np.ndarray): centroid
+        label (np.ndarray): dataset label
+        g_num (_type_): number of groups
     """
+
     # create graph
-    fig = plt.figure()
+    plt.figure()
+    colorlist = ["r", "g", "b", "c", "m", "y", "w"]
+    centroid = centroid.T
 
     if len(data) == 2:
         # create graph and plot
-        plt.plot(data[0], data[1], ".", label="dataset")
-        plt.set_xlabel("X")
-        plt.set_ylabel("f(X)")
+        for i in range(g_num):
+            plt.plot(
+                data[0, label == i],
+                data[1, label == i],
+                ".",
+                label="$G_{" + str(i) + "}$",
+                color=colorlist[i],
+            )
+        plt.plot(
+            centroid[0], centroid[1], "p",
+            label="centroid", color="k", markersize=10
+        )
+        plt.xlabel("$X_{1}$")
+        plt.ylabel("$X_{2}$")
+        plt.title(str(g_num) + " groups clustering")
         plt.legend()
 
     elif len(data) == 3:
-
         # create graph and plot
         ax = plt.subplot(projection="3d")
-        ax.plot(data[0], data[1], data[2], ".", c="b", label="dataset")
-        ax.set_xlabel("X1")
-        ax.set_ylabel("X2")
-        ax.set_zlabel("f(X)")
+        for i in range(g_num):
+            ax.plot(
+                data[0, label == i],
+                data[1, label == i],
+                data[2, label == i],
+                ".",
+                color=colorlist[i],
+                label="$G_{" + str(i) + "}$",
+            )
+        plt.plot(
+            centroid[0],
+            centroid[1],
+            centroid[2],
+            "p",
+            label="centroid",
+            color="k",
+            markersize=10,
+        )
+        ax.set_xlabel("$X_{1}$")
+        ax.set_ylabel("$X_{2}$")
+        ax.set_zlabel("$X_{3}$")
         ax.legend()
 
     else:
         raise ValueError(
-            "data dimension should in 2 or 3 but " + str(len(data_array[0]))
-        )
+            "data dimension should in 2 or 3 but " + str(len(data)))
 
-def read_csv(path:str) -> np.ndarray:
+
+def read_csv(path: str) -> np.ndarray:
     """read out csv to matrix
 
     Args:
@@ -47,7 +83,7 @@ def read_csv(path:str) -> np.ndarray:
     data_array = []
     with open(path, "r") as f:
         reader = csv.reader(f)
-        reader_data = next(reader)
+        next(reader)
         for line in reader:
             sub_group = []
             for value in line:
@@ -59,6 +95,7 @@ def read_csv(path:str) -> np.ndarray:
     data = data_array.T
 
     return data
+
 
 if __name__ == "__main__":
     pass
