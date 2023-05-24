ec78521a2765d89dd1d04b4c0c3bfc4145c24768
980055617 <y980055617@gmail.com>
2023-05-24 23:49:21 +0900

    update

--- ex5/t_kawanishi/main2.py
+++ ex5/t_kawanishi/main2.py
@@ -1,3 +1,31 @@
+"""Mel-Frequency Cepstrum Coefficients."""
+import argparse
+
 import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="This is a program to adapt MFCC."
+        )
+    parser.add_argument("path",help="sound file path")
+
+    #get args
+    args = parser.parse_args()
+
+    #read out sound
+    s_data, fs = sf.read(args.path)
+
+    #time
+    t = np.arange(0,len(s_data)) / fs
+
+    plt.figure()
+    plt.plot(t,s_data)
+    plt.show()
