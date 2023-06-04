56f78f7fa562323024ad12cbed5f97b1925438fa
kenjiro ide <jollydog1230@gmail.com>
2023-06-04 15:31:29 +0900

    done

--- ex5/d_ide/k_means.py
+++ ex5/d_ide/k_means.py
@@ -1,9 +1,18 @@
-'K-Means algorithm.'
+"K-Means algorithm."
 import numpy as np
-from numpy.random import Generator, PCG64
+from numpy.random import PCG64, Generator
 
 
 def random_cent2d(data, k):
+    """Generate first centroids.
+
+    Args:
+        data (ndarray): target data
+        k (int): number of divisions
+
+    Returns:
+        ndarray: array of centroids
+    """
     dimension = data.shape[1]
     random = np.zeros((k, dimension))
     rg_pcg = PCG64()
@@ -16,13 +25,22 @@ def random_cent2d(data, k):
     random_x = generator.uniform(min_x, max_x, k)
     random_y = generator.uniform(min_y, max_y, k)
     for i in range(0, k):
-        random[i,0] = random_x[i]
-        random[i,1] = random_y[i]
+        random[i, 0] = random_x[i]
+        random[i, 1] = random_y[i]
 
     return random
 
 
 def random_cent3d(data, k):
+    """Generate first centroids.
+
+    Args:
+        data (ndarray): target data
+        k (int): number of divisions
+
+    Returns:
+        ndarray: array of centroids
+    """
     dimension = data.shape[1]
     random = np.zeros((k, dimension))
     rg_pcg = PCG64()
@@ -38,14 +56,24 @@ def random_cent3d(data, k):
     random_y = generator.uniform(min_y, max_y, k)
     random_z = generator.uniform(min_z, max_z, k)
     for i in range(0, k):
-        random[i,0] = random_x[i]
-        random[i,1] = random_y[i]
-        random[i,2] = random_z[i]
+        random[i, 0] = random_x[i]
+        random[i, 1] = random_y[i]
+        random[i, 2] = random_z[i]
 
     return random
 
 
 def k_means2d(data, K, max_iters):
+    """Calculate kmeans of 2d.
+
+    Args:
+        data (ndarray): target data
+        K (int): number of divisions
+        max_iters (int): number of max loop
+
+    Returns:
+        ndarray: labels and centroids
+    """
 
     centroids = random_cent2d(data, K)
 
@@ -55,7 +83,9 @@ def k_means2d(data, K, max_iters):
         labels = np.argmin(distances, axis=1)
 
         # 新しいクラスタ中心を計算
-        new_centroids = np.array([data[labels == k].mean(axis=0) for k in range(K)])
+        new_centroids = np.array(
+            [data[labels == k].mean(axis=0) for k in range(K)]
+        )
 
         # クラスタ中心の変化が小さい場合は終了
         if np.allclose(centroids, new_centroids):
@@ -67,6 +97,16 @@ def k_means2d(data, K, max_iters):
 
 
 def k_means3d(data, K, max_iters):
+    """Calculate kmeans of 3d.
+
+    Args:
+        data (ndarray): target data
+        K (int): number of divisions
+        max_iters (int): number of max loop
+
+    Returns:
+        ndarray: labels and centroids
+    """
 
     centroids = random_cent3d(data, K)
 
@@ -76,7 +116,9 @@ def k_means3d(data, K, max_iters):
         labels = np.argmin(distances, axis=1)
 
         # 新しいクラスタ中心を計算
-        new_centroids = np.array([data[labels == k].mean(axis=0) for k in range(K)])
+        new_centroids = np.array(
+            [data[labels == k].mean(axis=0) for k in range(K)]
+        )
 
         # クラスタ中心の変化が小さい場合は終了
         if np.allclose(centroids, new_centroids):
--- ex5/d_ide/main.py
+++ ex5/d_ide/main.py
@@ -1,16 +1,22 @@
-'Main.'
+"Main."
 import argparse
+
+import librosa
+import librosa.display as lidi
 import matplotlib.pyplot as plt
 import numpy as np
+
 import k_means as km
+import mfcc
+
 
 def parse_args():
     """Retrieve variables from the command prompt."""
-    parser = argparse.ArgumentParser(description="Perform regression analysis.")
+    parser = argparse.ArgumentParser(description="Perform k-means and mfcc")
     parser.add_argument(
-        "--csv-file",
+        "--csv_file",
         type=str,
-        required=True,
+        default="../data1.csv",
         help="data csv file",
     )
     parser.add_argument(
@@ -18,6 +24,24 @@ def parse_args():
         type=int,
         default=2,
         help="number of cluster",
+    )
+    parser.add_argument(
+        "-nfft",
+        type=int,
+        default="512",
+        help="number of FFT points",
+    )
+    parser.add_argument(
+        "-ceps",
+        type=int,
+        default="12",
+        help="number of cepstrum",
+    )
+    parser.add_argument(
+        "--wav_file",
+        type=str,
+        default="../toujyo.wav",
+        help="data wav file",
     )
 
     return parser.parse_args()
@@ -36,6 +60,7 @@ def open_csv(file_path):
 
     return data_set
 
+
 def scat_plot2d(data, k):
     """Plot two-dimensional data.
 
@@ -53,8 +78,9 @@ def scat_plot2d(data, k):
     ax.set_title("K-Means 2d")
     ax.set_xlabel("$x_1$")
     ax.set_ylabel("$x_2$")
-    plt.savefig('result/Kmeans.png')
-    plt.show()
+    # plt.savefig('result/Kmeans.png')
+    # plt.show()
+    plt.close()
 
 
 def scat_plot3d(data, k):
@@ -71,7 +97,6 @@ def scat_plot3d(data, k):
     """
     labels, _ = km.k_means3d(data, k, 100)
 
-
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
     ax.set_xlabel("$x_1$")
@@ -79,7 +104,49 @@ def scat_plot3d(data, k):
     ax.set_zlabel("$x_3$")
     ax.set_title("K-Means 3d")
     ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=labels)
-    plt.savefig('result/Kmeans.png')
+    # plt.savefig('result/Kmeans.png')
+    # plt.show()
+
+
+def mel_plot(data, fs, mfcc, d, dd):
+    """Plot melspectrum.
+
+    Args:
+        data (nadarray): target data
+        fs (int): sampling frequency
+        mfcc (ndarray): mfcc data
+        d (ndarray): delta data
+        dd (ndarray): delta delta data
+    """
+    # plot original spectrogram
+    plt.subplot(111)
+    plt.specgram(data, fs)
+    plt.colorbar()
+    plt.xlabel("Time[s]")
+    plt.ylabel("Frequency[Hz]")
+    plt.savefig("result/original_spec.png")
+    plt.show()
+    plt.clf()
+    plt.close()
+
+    # plot mfcc, delta, deltadelta
+    plt.subplot(311)
+    lidi.specshow(mfcc.T)
+    plt.colorbar()
+    plt.ylabel("MFCC")
+
+    plt.subplot(312)
+    lidi.specshow(d.T)
+    plt.colorbar()
+    plt.ylabel("Delta")
+
+    plt.subplot(313)
+    lidi.specshow(dd.T)
+    plt.colorbar()
+    plt.ylabel("Delta delta")
+    plt.xlabel("time")
+    plt.savefig("result/mfcc.png")
+
     plt.show()
 
 
@@ -97,6 +164,15 @@ def main():
 
     elif data.shape[1] == 3:
         scat_plot3d(data, k)
+
+    data_wav, fs = librosa.load(args.wav_file, mono=True)
+    nfft = args.nfft
+    ceps = args.ceps
+
+    mfcc_data = mfcc.mfcc(data_wav, fs, 40, nfft, ceps)
+    d = mfcc.delta(mfcc_data)
+    dd = mfcc.delta(d)
+    mel_plot(data_wav, fs, mfcc_data, d, dd)
 
 
 if __name__ == "__main__":
