733e2bd4fb9dda124f5b23aa0ff042590a65f5a2
kenjiro ide <jollydog1230@gmail.com>
2023-08-14 13:52:25 +0900

    create mfcc image dataset

--- ex9/k_ide/mfcc.py
+++ ex9/k_ide/mfcc.py
@@ -1,15 +1,36 @@
+import os
 import numpy as np
+import torch
+import torchaudio
+import torchmetrics
+import pandas as pd
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 import soundfile as sf
 
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-x, fs = sf.read('../dataset/train/george_0.wav')
-mfccs = librosa.feature.mfcc(y=x, sr=fs)
-fig, ax = plt.subplots(ncols=1)
-img = librosa.display.specshow(mfccs, sr=fs, x_axis='time')
-ax.set(title='MFCC')
-ax.label_outer()
-fig.colorbar(img, ax=ax)
-plt.show()
+training = pd.read_csv(os.path.join(root, "training.csv"))
+train_path = training["path"].values
+train_lebel = training['label'].values
+n_mfcc = 13
+datasize = len(train_path)
+features = torch.zeros(datasize, n_mfcc)
+transform = torchaudio.transforms.MFCC(n_mfcc=13, melkwargs={'n_mels':64, 'n_fft':512})
+
+for i, path in enumerate(train_path):
+    # data.shape==(channel,time)
+    if i == 100:
+        data, fs = torchaudio.load(os.path.join(root, path))
+        #features[i] = torch.mean(transform(data[0]), axis=1)
+        data = data.flatten()
+        x = data.to('cpu').detach().numpy().copy()
+        #print(data)
+        #import pdb; pdb.set_trace()
+        mfccs = librosa.feature.mfcc(y=x, sr=fs)
+        fig, ax = plt.subplots(ncols=1)
+        img = librosa.display.specshow(mfccs, sr=fs, x_axis='time')
+        ax.set(title='MFCC')
+        fig.colorbar(img, ax=ax)
+        plt.show()
